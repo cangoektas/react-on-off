5aaea07ed505dabf21c47449d514f0723b025863
Can Göktas <can.goektas@gmail.com>
2018-06-27 17:02:09 +0200

    Simplify build script

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import minify from "rollup-plugin-babel-minify";
+
+const getBabelConfig = require("./get-babel-config");
+
+const mainConfig = {
+  input: "src/index.js",
+  external: ["react", "prop-types"],
+  plugins: [babel(getBabelConfig({ modules: false })), resolve()],
+  output: [
+    {
+      file: "dist/index.esm.js",
+      format: "esm"
+    },
+    {
+      file: "dist/index.umd.js",
+      format: "umd",
+      name: "OnOff",
+      globals: {
+        react: "React",
+        "prop-types": "PropTypes"
+      }
+    }
+  ],
+  watch: {
+    include: "src/**"
+  }
+};
+const umdConfig = {
+  input: "src/index.js",
+  external: ["react", "prop-types"],
+  plugins: [
+    babel(getBabelConfig({ modules: false })),
+    resolve(),
+    minify({ comments: false, sourceMap: false })
+  ],
+  output: {
+    file: "dist/index.umd.min.js",
+    format: "umd",
+    name: "OnOff",
+    globals: {
+      react: "React"
+    }
+  }
+};
+
+export default [mainConfig, umdConfig];
